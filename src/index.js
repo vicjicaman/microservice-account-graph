989e6525be88a6d29b4127c865d151bcbefaaf94
Victor Jimenez <vic.jicama@gmail.com>
2019-09-17 18:22:45 +0200

    Add recconect functionality for cache and data services

--- src/index.js
+++ src/index.js
@@ -1,41 +1,76 @@
-require('dotenv').config()
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const {
-  makeExecutableSchema
-} = require('graphql-tools');
-const {
-  schema: rootSchema,
-  resolvers: rootResolvers
-} = require('./schema');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const { makeExecutableSchema } = require("graphql-tools");
+const { schema: rootSchema, resolvers: rootResolvers } = require("./schema");
+const mongoose = require("mongoose");
 
+export function wait(timeout) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, timeout);
+  });
+}
 
-const INTERNAL_URL_DATA = process.env['INTERNAL_URL_DATA'];
-const ROUTE_GRAPH = process.env['ROUTE_GRAPH'];
-const INTERNAL_PORT_GRAPH = process.env['INTERNAL_PORT_GRAPH'];
+const INTERNAL_URL_DATA = process.env["INTERNAL_URL_DATA"];
+const ROUTE_GRAPH = process.env["ROUTE_GRAPH"];
+const INTERNAL_PORT_GRAPH = process.env["INTERNAL_PORT_GRAPH"];
 
-console.log("Connect to data service...")
-mongoose.connect('mongodb://' + INTERNAL_URL_DATA, {
-  useNewUrlParser: true
-});
+(async () => {
+  let db = null;
+  let connected = false;
 
-var app = express();
+  while (!connected) {
+    try {
+      console.log("Connect to data service...");
+      await mongoose.connect("mongodb://" + INTERNAL_URL_DATA, {
+        useNewUrlParser: true,
+        reconnectTries: 3,
+        reconnectInterval: 100
+      });
 
-const schema = makeExecutableSchema({
-  typeDefs: rootSchema,
-  resolvers: rootResolvers
-});
-
-app.use(ROUTE_GRAPH, graphqlHTTP(request => ({
-  schema: schema,
-  graphiql: true,
-  context: {
-    request
+      mongoose.connection.on("disconnected", () => {
+        console.log("-> lost connection");
+      });
+      mongoose.connection.on("reconnect", () => {
+        console.log("-> reconnected");
+      });
+      mongoose.connection.on("connected", () => {
+        console.log("-> connected");
+      });
+      mongoose.connection.on("reconnectFailed", () => {
+        console.log("-> gave up reconnecting");
+        process.exit(17);
+      });
+      connected = true;
+    } catch (e) {
+      console.log("DATA_ERROR:  " + e.toString());
+      await wait(2500);
+    }
   }
-})));
-app.listen(INTERNAL_PORT_GRAPH, () => console.log('Inner account GraphQL running...'));
 
+  var app = express();
+
+  const schema = makeExecutableSchema({
+    typeDefs: rootSchema,
+    resolvers: rootResolvers
+  });
+
+  app.use(
+    ROUTE_GRAPH,
+    graphqlHTTP(request => ({
+      schema: schema,
+      graphiql: true,
+      context: {
+        request
+      }
+    }))
+  );
+  app.listen(INTERNAL_PORT_GRAPH, () =>
+    console.log("Inner account GraphQL running...")
+  );
+})();
 
 function shutdown(signal) {
   return async function(err) {
@@ -48,12 +83,12 @@ function shutdown(signal) {
     mongoose.connection.close();
 
     setTimeout(() => {
-      process.exit(
-        err ?
-        1 :
-        0);
+      process.exit(err ? 1 : 0);
     }, 500).unref();
   };
 }
 
-process.on('SIGTERM', shutdown('SIGTERM')).on('SIGINT', shutdown('SIGINT')).on('uncaughtException', shutdown('uncaughtException'));
+process
+  .on("SIGTERM", shutdown("SIGTERM"))
+  .on("SIGINT", shutdown("SIGINT"))
+  .on("uncaughtException", shutdown("uncaughtException"));
